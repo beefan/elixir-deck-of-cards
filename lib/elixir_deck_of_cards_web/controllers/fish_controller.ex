a4edc0680ccc0d41acbc26984b0161f0e081ac77
Brandon Fannin <bfannin13@gmail.com>
2020-05-24 14:03:41 -0400

    data flowing from postgres to elixir template

--- lib/elixir_deck_of_cards_web/controllers/fish_controller.ex
+++ lib/elixir_deck_of_cards_web/controllers/fish_controller.ex
@@ -1,7 +1,8 @@
 defmodule ElixirDeckOfCardsWeb.FishController do
   use ElixirDeckOfCardsWeb, :controller
-
+  import Ecto.Query, only: [from: 2]
   def index(conn, _params) do 
-    render(conn, "index.html")
+    cards = ElixirDeckOfCards.Repo.all(from c in "deck", select: [c.value, c.suit, c.color])
+    render(conn, "index.html", cards: cards)
   end
 end
